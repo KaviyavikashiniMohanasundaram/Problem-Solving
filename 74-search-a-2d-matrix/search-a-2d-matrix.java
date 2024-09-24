0fdd3540d316bdf28f38b0bb6bcbcc34bf727695
Kaviyavikashini M <mkavivika05@gmail.com>
2024-09-24 12:02:51 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (81.86%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -10,16 +10,15 @@ class Solution
     {
       int mid = (low + high) / 2;
       int mid_value = matrix[mid / cols][mid % cols];
-        
         if (mid_value == target)
             return true;
         else if(mid_value < target)
             low = mid + 1;
         else
             high = mid - 1;
-    }
-    
+    }   
     return false;
     }
 }
+
 
