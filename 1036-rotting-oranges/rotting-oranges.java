d267184ccf9ddc5f7d311fadc839e848348e647b
Kaviyavikashini M <mkavivika05@gmail.com>
2024-11-04 16:07:11 +0530

    Time: 2 ms (71.52%) | Memory: 42.7 MB (23.71%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -1,48 +1,61 @@
-class Solution {
-    public int orangesRotting(int[][] grid) {
-        int m = grid.length;
-        int n = grid[0].length;
-        int[][] visited = grid;
-        Queue<int[]> q = new LinkedList<>();
-        int countFreshOrange = 0;
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (visited[i][j] == 2) {
-                    q.offer(new int[] {i, j});
-                }
-                if (visited[i][j] == 1) {
-                    countFreshOrange++;
-                }
-            }
-        }
-        if (countFreshOrange == 0)
-            return 0;
-        if (q.isEmpty())
-            return -1;
-        
-        int minutes = -1;
-        int[][] dirs = {{1, 0},{-1, 0},{0, -1},{0, 1}};
-        while (!q.isEmpty()) {
-            int size = q.size();
-            while (size-- > 0) {
-                int[] cell = q.poll();
-                int x = cell[0];
-                int y = cell[1];
-                for (int[] dir : dirs) {
-                    int i = x + dir[0];
-                    int j = y + dir[1];
-                    if (i >= 0 && i < m && j >= 0 && j < n && visited[i][j] == 1) {
-                        visited[i][j] = 2;
-                        countFreshOrange--;
-                        q.offer(new int[] {i, j});
-                    }
-                }
-            }
-            minutes++;
-        }
-        
-        if (countFreshOrange == 0)
-            return minutes;
-        return -1;
+class Pair
+{
+    int row;
+    int col;
+    int level;
+    Pair(int _row,int _col,int _level)
+    {
+        this.row=_row;
+        this.col = _col;
+        this.level = _level;
+    }//Pair(2,3,9)
+}
+class Solution
+{
+    public int orangesRotting(int [][] grid)
+    {
+       Queue<Pair> q = new LinkedList<>();
+       int m = grid.length;
+       int n = grid[0].length;
+       int fr =0;
+       int [][] vis = new int[m][n];
+       for(int i=0;i<m;i++)
+       {
+           for(int j=0;j<n;j++)
+           {
+               if(grid[i][j]==2)
+               {
+                   q.offer(new Pair(i,j,0));
+                   vis[i][j]=1;
+               }
+               if(grid[i][j]==1) fr++;
+           }
+       }
+       int max_level = 0;
+       int [] drow ={0,1,0,-1};
+       int [] dcol ={1,0,-1,0};
+       while(!q.isEmpty())
+       {
+           int r = q.peek().row;
+           int c = q.peek().col;
+           int curr_level = q.peek().level;
+           q.poll();
+           max_level = Math.max(curr_level,max_level);
+           for(int i=0;i<4;i++)
+           {
+               int nrow= r+drow[i];
+               int ncol = c + dcol[i];
+               if(nrow>=0 && nrow<m && ncol>=0 && ncol<n && vis[nrow][ncol]==0 && grid[nrow][ncol]==1)
+               {
+                   q.offer(new Pair(nrow,ncol,curr_level+1));
+                   vis[nrow][ncol]=1;
+                   fr--;
+               }
+               
+           }
+           
+       }
+       if(fr!=0) return -1;
+       return max_level;
     }
 }
